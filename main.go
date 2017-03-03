546f0fe5290a35439ec239a735c6a2ee5996f72e
Andras Belicza <iczaaa@gmail.com>
2017-03-03 15:07:44 +0100

    Initialized event system, exiting on QuitEvent.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Package main is the runnable app of the Bouncing balls demo app.
+// Package main is the Bouncing balls demo app.
 package main
 
 import (
@@ -18,20 +18,25 @@ func main() {
 
 func run() (exitCode int) {
 	var err error
+	fail := func(op string, exitCode int) int {
+		log.Printf("could not %s: %v", op, err)
+		return exitCode
+	}
+
 	if err = sdl.Init(sdl.INIT_VIDEO); err != nil {
-		return fail("init SDL video", err, 1)
+		return fail("init SDL video", 1)
 	}
 	defer sdl.Quit()
 
 	var win *sdl.Window
 	if win, err = sdl.CreateWindow(title, sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED, 800, 600, sdl.WINDOW_SHOWN); err != nil {
-		return fail("create window", err, 2)
+		return fail("create window", 2)
 	}
 	defer win.Destroy()
 
 	var r *sdl.Renderer
 	if r, err = sdl.CreateRenderer(win, -1, sdl.RENDERER_ACCELERATED); err != nil {
-		return fail("create renderer", err, 3)
+		return fail("create renderer", 3)
 	}
 	defer r.Destroy()
 
@@ -39,12 +44,26 @@ func run() (exitCode int) {
 	r.SetDrawColor(150, 150, 150, 255)
 	r.DrawRect(&sdl.Rect{X: 0, Y: 0, W: 800, H: 600})
 	r.Present()
-	sdl.Delay(3000)
+
+	for {
+		e := sdl.WaitEvent()
+		if quit := handleEvent(e); quit {
+			break
+		}
+	}
 
 	return 0
 }
 
-func fail(op string, err error, exitCode int) int {
-	log.Printf("could not %s: %v", op, err)
-	return exitCode
+func handleEvent(event sdl.Event) (quit bool) {
+	switch e := event.(type) {
+	case *sdl.QuitEvent:
+		return true
+	// Ignored events:
+	case *sdl.MouseMotionEvent:
+	default:
+		log.Printf("event: %T", e)
+	}
+
+	return false
 }
